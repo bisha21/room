7226d20f2e0aa66c1637c4dd172f62afed1f4008
[bisha21] <[timilsinab22bi@gmail.com]>
2025-06-04 13:17:52 +0545

    change ur;

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import io from 'socket.io-client';
 import ChatRoom from './ChatApp';
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('https://room-vb96.onrender.com/');
 
 const App = () => {
   const [username, setUsername] = useState('');
@@ -17,37 +17,32 @@ const App = () => {
 
   return (
     <>
-{
- !showChat &&(
-  <div className="join-container">
-      <form className="join-form" onSubmit={handleJoin}>
-        <h2>Join Chat Room</h2>
+      {!showChat && (
+        <div className="join-container">
+          <form className="join-form" onSubmit={handleJoin}>
+            <h2>Join Chat Room</h2>
 
-        <input
-          type="text"
-          placeholder="Enter your name"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="number"
-          placeholder="Enter your room number"
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-          required
-        />
+            <input
+              type="text"
+              placeholder="Enter your name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              type="number"
+              placeholder="Enter your room number"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+              required
+            />
 
-        <button type="submit">Join</button>
-      </form>
+            <button type="submit">Join</button>
+          </form>
+        </div>
+      )}
 
-    </div>
- )
-}
-    
       {showChat && <ChatRoom socket={socket} username={username} room={room} />}
-
-
     </>
   );
 };
